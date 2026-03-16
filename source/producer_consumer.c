ad40a44d8d4ae7da3237a4ee5ff9db53d139dc0e
Gianni Belizaire <57324798+758gianni@users.noreply.github.com>
2026-03-16 19:30:44 -0300

    change child process order

--- source/producer_consumer.c
+++ source/producer_consumer.c
@@ -4,16 +4,7 @@
 #include <sys/wait.h>
 
 int main() {
-    printf("\nStarting consumer and producer processes...\n\n");
-
-    // Fork child process for consumer
-    pid_t consumer_pid = fork();
-
-    if (consumer_pid == 0) {
-        execl("./executables/consumer", "consumer", NULL);
-        perror("execl consumer failed");
-        exit(1);
-    }
+    printf("\nStarting producer and consumer processes...\n\n");
 
     // Fork child process for producer
     pid_t producer_pid = fork();
@@ -24,11 +15,20 @@ int main() {
         exit(1);
     }
 
+    // Fork child process for consumer
+    pid_t consumer_pid = fork();
+
+    if (consumer_pid == 0) {
+        execl("./executables/consumer", "consumer", NULL);
+        perror("execl consumer failed");
+        exit(1);
+    }
+
     // Wait for both children to complete
     int status;
 
-    waitpid(consumer_pid, &status, 0);
     waitpid(producer_pid, &status, 0);
+    waitpid(consumer_pid, &status, 0);
 
     return 0;
 }
